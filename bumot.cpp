196f8334c5cedc5f7f35f93fe22e93407d9f5767
Leonardo Cecchi <leonardoce@interfree.it>
2012-06-16 09:49:29 +0200

    work

--- bumot.cpp
+++ bumot.cpp
@@ -1,5 +1,9 @@
 #include <wx/wx.h>
+#include <wx/html/htmlwin.h>
 #include "bumotstrings.hpp"
+
+const char * const Fin_String = "Eng/RapMoz: ";
+const char * const Fin_Button_String = "Fin!";
 
 /**
  * C e u sip rowe f u rap lin lie bu-mot
@@ -11,11 +15,33 @@ public:
 };
 
 class BuMotFrame : public wxFrame {
+private:
+  wxTextCtrl *txtFin;
+  wxHtmlWindow *contents;
+  wxButton *butFin;
 public:
   BuMotFrame(const wxString &title);
 };
 
 BuMotFrame::BuMotFrame(const wxString &title) : wxFrame(NULL, -1, title) {
+  wxBoxSizer *kaSizer = new wxBoxSizer(wxVERTICAL);
+
+  wxPanel *finPanel = new wxPanel(this);
+  wxBoxSizer *panelSizer = new wxBoxSizer(wxHORIZONTAL);
+  
+  panelSizer->Add(new wxStaticText(finPanel, -1, Fin_String), 0, 0);
+  txtFin = new wxTextCtrl(finPanel, -1, "");
+  panelSizer->Add(txtFin, 1, 0, 0);
+  butFin = new wxButton(finPanel, -1, Fin_Button_String);
+  panelSizer->Add(butFin, 0, 0);
+  finPanel->SetSizer(panelSizer);
+
+  kaSizer->Add(finPanel, 0, wxEXPAND | wxALL, 0);
+
+  contents = new wxHtmlWindow(this);
+  kaSizer->Add(contents, 1, wxEXPAND | wxALL, 0);
+
+  SetSizer (kaSizer);
 }
 
 bool BuMotApp::OnInit() {
