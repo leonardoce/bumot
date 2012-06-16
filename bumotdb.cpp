72fb26df4705202b97b0fca34feb61cfa2dd1e61
Leonardo Cecchi <leonardoce@interfree.it>
2012-06-16 10:16:38 +0200

    work

--- bumotdb.cpp
+++ bumotdb.cpp
@@ -1,4 +1,6 @@
 #include "bumotdb.hpp"
+#include <boost/format.hpp>
+#include <boost/foreach.hpp>
 
 BuMotDb::BuMotDb() 
 {
@@ -12,6 +14,48 @@ void BuMotDb::Init() {
   if(!dbConnection) {
     dbConnection.reset(new sqlite_connection("bumotdb.db"));
   }
+}
+
+std::string stosingle(const std::string &query) {
+  std::string result;
+
+  result += '\'';
+
+  BOOST_FOREACH(char c, query) {
+    if (c=='\'') {
+      result += '\'';
+      result += '\'';      
+    } else {
+      result += c;
+    }
+  }
+
+  result += '\'';
+
+  return result;
+}
+
+std::vector<BuMotDb::BuMotRecord> BuMotDb::Find(const std::string &findStr) {
+  std::vector<BuMotDb::BuMotRecord> result;
+
+  std::string sql = "SELECT rap, mot FROM dict WHERE rap " 
+    "ILIKE %1% OR mot ILIKE %2%";
+  
+  std::string formattedSql = 
+    str( boost::format(sql) 
+	 % stosingle("%" + findStr + "%") 
+	 % stosingle("%" + findStr + "%") );
+  
+  std::tr1::shared_ptr<db_recordset> rs = 
+    dbConnection->sql_retrieve(sql);
+  
+  while(rs->next()) {
+    BuMotDb::BuMotRecord record(rs->get_string_field(0), 
+				rs->get_string_field(1));
+    result.push_back(record);
+  }
+
+  return result;
 }
 
 BuMotDb& CurrentDatabase()
--- bumotdb.hpp
+++ bumotdb.hpp
@@ -14,7 +14,20 @@ public:
   const char *what() { return theMessage.c_str(); };
 };
 
+
 class BuMotDb {
+public:
+  class BuMotRecord {
+  private:
+    std::string rapmot;
+    std::string engmot;
+  public:
+    const std::string &get_rapmot() const { return rapmot; };
+    const std::string &get_engmot() const { return engmot; };
+    BuMotRecord(const std::string &theRapMot, const std::string &theEngMot)
+      : rapmot(theRapMot), engmot(theEngMot) {};
+  };
+
 private:
   std::tr1::shared_ptr<db_connection> dbConnection;
 
@@ -23,6 +36,7 @@ public:
   ~BuMotDb() throw ();
 
   void Init();
+  std::vector<BuMotRecord> Find(const std::string &findStr);
 };
 
 BuMotDb& CurrentDatabase();
