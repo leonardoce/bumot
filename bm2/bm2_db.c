7ce85bc012bd57315a574cbd841c1f43d9d8d6c2
Leonardo Cecchi <leonardoce@interfree.it>
2012-06-20 16:35:43 +0200

    working

--- bm2/bm2_db.c
+++ bm2/bm2_db.c
@@ -1,48 +1,140 @@
 #include <stdbool.h>
 #include <stdlib.h>
+#include "bstrlib.h"
 #include "bm2_db.h"
 #include "sqlite3.h"
-#include "bstrlib.h"
+#include "logging.h"
 
 struct data_connection {
-  sqlite3 *db;
+	sqlite3 *db;
+};
+
+struct result_set {
+	sqlite3_stmt *stmt;
 };
 
 data_connection *new_data_connection(const char *connString) {
-  data_connection *dc;
+	data_connection *dc;
 
-  dc = (data_connection *)malloc(sizeof(struct data_connection));
-  if (dc==NULL) return NULL;
+	dc = (data_connection *)malloc(sizeof(struct data_connection));
+	if (dc==NULL) return NULL;
 
-  if (sqlite3_open (connString, &dc->db)!=SQLITE_OK) {
-    sqlite3_close(dc->db);
-    free (dc);
-    return NULL;
-  }
+	if (sqlite3_open (connString, &dc->db)!=SQLITE_OK) {
+		sqlite3_close(dc->db);
+		free (dc);
+		return NULL;
+	}
 
-  return dc;
+	return dc;
 }
 
 bool check_data_connection(data_connection *dc) {
-  // Una dataconnection sqlite, se viene allocata,
-  // va sempre benone
-  return true;
+	// Una dataconnection sqlite, se viene allocata,
+	// va sempre benone
+	return true;
 }
 
 void delete_data_connection(data_connection *dc) {
-  sqlite3_close(dc->db);
-  free(dc);
+	sqlite3_close(dc->db);
+	free(dc);
 }
 
 void sql_exec(data_connection *dc, const char *sql) {
-  bstring errmsg;
+	if (sqlite3_exec(dc->db, sql, NULL, NULL, NULL)!=SQLITE_OK) {
+		// Errore in esecuzione di una query
+		log_error("Errore SQL: ", sql);
+	}
+}
 
-  if (sqlite3_exec(dc->db, sql, NULL, NULL, NULL)!=SQLITE_OK) {
-    errmsg = bfromcstr("Errore SQL: ");
-    bcatcstr(errmsg, sql);
+void sql_into(data_connection *dc, const char *sql, bstring to) {
+	sqlite3_stmt *stmt;
 
-    // Errore in esecuzione di una query
-    log_error(bstr2cstr(errmsg, ' '));
-    bdestroy(errmsg);
-  }
+	if (sqlite3_prepare (dc->db, sql, strlen(sql), &stmt, NULL)!=SQLITE_OK) {
+		// Errore nella query
+		sqlite3_finalize(stmt);
+		log_error("Errore SQL: ", sql, NULL);
+		return;
+	}
+
+	btrunc(to, 0);
+
+	while (true) {
+		int status = sqlite3_step(stmt);
+		if (status==SQLITE_ROW) {
+			const char *contents = (const char *)sqlite3_column_text(stmt, 0);
+			bcatcstr(to, contents);
+		}
+	}
+
+	sqlite3_finalize(stmt);
+}
+
+result_set *sql_retrieve(data_connection *dc, const char *sql) {
+	sqlite3_stmt *stmt;
+	int status;
+	result_set *rs;
+
+	rs = (result_set *)malloc(sizeof(struct result_set));
+	rs->stmt = NULL;
+
+	status = sqlite3_prepare (dc->db, sql, strlen(sql), &stmt, NULL);
+	if (status!=SQLITE_OK) {
+		sqlite3_finalize(stmt);
+		log_error("Errore SQL: ", sql, NULL);
+		return rs;
+	}
+
+	rs->stmt = stmt;
+	return rs;
+}
+
+int rs_cols(result_set *rs)
+{
+	if (rs->stmt==NULL) {
+		return 0;
+	}
+
+	return sqlite3_column_count(rs->stmt);
+}
+
+bool rs_next(result_set *rs)
+{
+	if (rs->stmt==NULL) {
+		return false;
+	}
+
+	while (true) {
+		int status = sqlite3_step(rs->stmt);
+		if (status==SQLITE_ROW) {
+			return true;
+		} else if (status==SQLITE_DONE) {
+			return false;
+		} else {
+			log_error("Error stepping result set!", NULL);
+			return false;
+		}
+	}
+}
+
+void rs_get_string(result_set *rs, int n, bstring to) {
+	btrunc(to, 0);
+
+	if (rs->stmt==NULL) {
+		return;
+	}
+
+	const char *contents = (const char *)sqlite3_column_text(rs->stmt, n);
+	if (contents==NULL) {
+		return;
+	}
+
+	bcatcstr(to, contents);
+}
+
+void rs_destroy(result_set *rs) {
+	if (rs->stmt!=NULL) {
+		sqlite3_finalize(rs->stmt);
+	}
+
+	free(rs);
 }
--- bm2/bm2_db.h
+++ bm2/bm2_db.h
@@ -4,6 +4,9 @@
 struct data_connection;
 typedef struct data_connection data_connection;
 
+struct result_set;
+typedef struct result_set result_set;
+
 /**
  * Crea una nuova data connection
  * @param connString Nome della data connection
@@ -26,5 +29,32 @@ void delete_data_connection(data_connection *dc);
  * @return True se va tutto bene
  */
 void sql_exec(data_connection *dc, const char *sql);
+
+/**
+ * Esegue una query e prende il result set
+ * @return Il result set
+ */
+result_set *sql_retrieve(data_connection *dc, const char *sql);
+
+/**
+ * Quante sono le colonne in questo result set?
+ */
+int rs_cols(result_set *rs);
+
+/**
+ * C'e' ancora una riga?
+ */
+bool rs_next(result_set *rs);
+
+/**
+ * Prende i dati della colonna "n" e li mette nella
+ * bstring "to"
+ */
+void rs_get_string(result_set *rs, int n, bstring to);
+
+/**
+ * Dealloca tutte le risorse del result set
+ */
+void rs_destroy(result_set *rs);
 
 #endif
